a2a60404978723371f62e1f552f443fa9c3b1343
Rehan Siraj <rsiraj@r2hstudent.org>
2022-01-05 13:54:53 -0500

    changed order of prompts in medium

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ function divide(number1, number2) {
 //VERY HARD: Create a simple calculator that prompts the user for a number, an operator (either +, -, * or /) and another number, and then uses the functions created in the hard challenge to output the value in sentence form. Example output: "3 + 4 = 7"
 function veryhard() {
     let number1 = Number(prompt("Please enter the first number"));
-    let number2 = Number(prompt("Please enter the second number"));
     let mathOperation = prompt("Please enter +, -, *, or /");
+    let number2 = Number(prompt("Please enter the second number"));
 
 
     if (mathOperation === "+") {
